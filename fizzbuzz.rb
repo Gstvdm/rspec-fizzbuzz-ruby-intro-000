8eb2630e60bf673302a26964e5980c1c826db238
Gustavo Pinheiro <gstvdm@gmail.com>
2017-11-29 02:47:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,17 @@
 def fizzbuzz(number)
-  if (number % 3) == 0
+
+  if (number % 15) == 0
+    return "FizzBuzz"
+  end
+  elseif (number % 3) == 0
     return "Fizz"
   end
   elseif (number % 5) == 0
     return "Buzz"
+    
   end
-
   
+
 
 
 
